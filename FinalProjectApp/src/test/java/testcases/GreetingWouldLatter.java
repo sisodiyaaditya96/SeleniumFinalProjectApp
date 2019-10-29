6791f6bf326b6fc278a1d54c7fd66e6171ee200f
Aadi <Aadi@DESKTOP-PPO8L1H>
2019-10-29 08:37:00 +0530

    new class added related to greeting message

---
+++ FinalProjectApp/src/test/java/testcases/GreetingWouldLatter.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class GreetingWouldLatter {
+	@Test
+	public void printHelloMessage() throws Exception {
+		System.out.println("this is greetingWouldLatter Class Message | ");
+	}
+}
